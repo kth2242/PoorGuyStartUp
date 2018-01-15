66add4bd8867b8a2f2bf2fed72597300bcbcc5e1
Picbridge <34910988+Picbridge@users.noreply.github.com>
2018-01-16 01:51:37 +0900

    DayNightSystem
    Included:
    DayNightManager.cs
    DayNightManager empty component
    
    Changed:
    Component Materials(default->sprite diffuse)
    
    Deleted:
    None

---
+++ Assets/Scripts/DayNightManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightManager : MonoBehaviour
+{
+    private const float DAYDURATION = 5f;
+
+    [SerializeField]
+    private Light Sun;
+    private float sunIntensityLvl = 1;
+    [SerializeField]
+    private Light pointLight;
+    private float plIntensityLvl = 0;
+
+    WaitForSeconds longTerm = new WaitForSeconds(DAYDURATION);
+    WaitForSeconds shortTerm = new WaitForSeconds(DAYDURATION);
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(startNightCycle());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Sun.intensity = sunIntensityLvl;
+        pointLight.intensity = plIntensityLvl;
+    }
+
+    IEnumerator startNightCycle()
+    {
+        yield return longTerm;
+        while (sunIntensityLvl > 0f && plIntensityLvl < 1f)
+        {
+            yield return shortTerm;
+            sunIntensityLvl -= (1f / DAYDURATION);
+            plIntensityLvl += (1f / DAYDURATION);
+        }
+        yield return StartCoroutine(startDayCycle());
+    }
+ 
+    IEnumerator startDayCycle()
+    {
+        yield return longTerm;
+        while (sunIntensityLvl < 1f && plIntensityLvl > 0f)
+        {
+            yield return shortTerm;
+            sunIntensityLvl += (1f / DAYDURATION);
+            plIntensityLvl -= (1f / DAYDURATION);
+        }
+        yield return StartCoroutine(startNightCycle());
+    }
+}
